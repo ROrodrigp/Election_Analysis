11a6d67975b86be4c83fc2c8caa20b9495756d8b
Rodrigo Alan García Pérez <rodrigo.agp96@gmail.com>
2022-01-12 17:57:00 -0600

    Adding the election audit code

--- PyPoll.py
+++ PyPoll.py
@@ -7,22 +7,57 @@
 
 # Import the datetime class from the datetime module.
 import datetime as dt 
+import csv
+import os 
 # Use the now() attribute on the datetime class to get the present time.
 now = dt.datetime.now()
 # Print the present time.
 print("The time right now is ", now)
 
-import csv
-import os 
 
 #file_to_load = "Resources/election_results.csv"
 file_to_load = os.path.join("Resources", "election_results.csv")
+
+total_votes = 0 
+candidate_options = []
+candidate_votes = {}
+winning_candidate = ""
+winning_count = 0 
+winning_percentage = 0 
+
 with open(file_to_load) as election_data:
     file_reader = csv.reader(election_data)
     #Each row in the CSV file was printed out as a list.
     header = next(file_reader)
-    print(header)
+    print(f'The headers are {header}')
 
+    for row in file_reader:
+        total_votes += 1
+        candidate_name = row[2]
+        if candidate_name not in candidate_options:
+            candidate_options.append(candidate_name)
+            candidate_votes[candidate_name] = 0 
+        candidate_votes[candidate_name]+= 1
+    print(total_votes)
+    print(candidate_options)
+    print(candidate_votes)
+
+for candidate in candidate_options:
+    
+    votes_percentages = float(candidate_votes[candidate]) / float(total_votes) * 100 
+    print(f'{candidate}: received {votes_percentages:.1f}% and {candidate_votes[candidate]:,} votes')
+    if (candidate_votes[candidate] > winning_count) and (votes_percentages > winning_percentage):
+        winning_count = candidate_votes[candidate]
+        winning_percentage = votes_percentages
+        winning_candidate = candidate
+winning_candidate_summary = (
+    f'-------------------------\n'
+    f'Winner: {winning_candidate}\n'
+    f'Winning Vote Count {winning_count:,}\n'
+    f'Winning Percentage {winning_percentage:.1f}%\n'
+    f'-------------------------\n'
+)
+print(winning_candidate_summary)
 file_to_save = "Analysis/election_analysis.txt"
 #outfile = open(file_to_save, "w")
 #outfile.write("Hello world")
