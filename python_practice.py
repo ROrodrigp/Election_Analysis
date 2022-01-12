a9ba95bf4c66c639fbdc07e48c3100016436707d
Rodrigo Alan García Pérez <rodrigo.agp96@gmail.com>
2022-01-12 10:41:23 -0600

    Practica de python

---
+++ python_practice.py
@@ -0,0 +1,13 @@
+print('Hello world')
+
+name = 'Rodrigo'
+country = 'Mexico'
+age = 25
+satisfied = True
+hourly_wage = 10
+daily_wage = 8 * hourly_wage
+print(f'I am {name} and I live in {country}')
+print(f'I am {age} years old')
+print(f'I make {daily_wage} a day')
+print(f'Are you satisfied with your current wage? {satisfied}')
+
